b81a4dedfddd30bd50de011c70651d9a5ad7cab2
Molchanova Tatyana <mtuspeh@gmail.com>
2019-10-08 09:03:32 +0300

    4 commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,50 @@
 	
 	var arr = ['1', '4', '54', '-32', '0', '21', '9', '100'];
 
-	var res = arr.map((el)=> parseInt(el)).reduce((max, value) => Math.max(max, value), 0);
+	// var res = arr.map((el)=> parseInt(el)).reduce((max, value) => Math.max(max, value), 0);
 
-	console.log(res);
+	// console.log(res);
 
+	var res = arr.map(function (el) {
+		return parseInt(el);
+	  }).reduce(function (max, value) {
+		//   console.log('max', max)
+		  
+		  if (!(value % 2))  
+			console.log('value2', value)
+			//   return value
+			  
+			  return Math.max(max, value);
+		  
+	  }, 0);
 
-
-
-	
+		console.log(res);
 
 })();
 
 
+// 4 decision 
+
+// var res = arr.map(function (el) {
+// 	return parseInt(el);
+//   }).reduce(function (max, value) {
+// 	//   console.log('max', max)
+	  
+// 	  if (!(value % 2))  
+// 		console.log('value2', value)
+// 		//   return value
+		  
+// 		  return Math.max(max, value);
+	  
+//   }, 0);
+
+// 	console.log(res);
 
 
 
+// 3 decision 
 
+// var res = arr.map((el)=> parseInt(el)).reduce((max, value) => Math.max(max, value), 0);
 
 
 // 2 decision
